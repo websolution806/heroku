a39b39b1b33007398fb1a75f44feea88a7291512
baseline <websolution806@gmail.com>
2017-09-12 02:49:29 +0530

    Update download.php

--- download.php
+++ download.php
@@ -3,7 +3,7 @@ $actual_link =  "http://" . $_SERVER['SERVER_NAME'];
 //echo $actual_link;
 
 $file_name = 'contacts.csv';
-$file_url = 'https://instagram-profiles.herokuapp.com' . $file_name;
+$file_url = 'http://" . $_SERVER["SERVER_NAME"]' . $file_name;
 header('Content-type: text/csv');
 //header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"".$file_name."\""); 
