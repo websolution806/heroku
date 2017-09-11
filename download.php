6c5a6080a4e39e6ad863db38d18febc166a0bc48
baseline <websolution806@gmail.com>
2017-09-12 02:55:10 +0530

    Update download.php

--- download.php
+++ download.php
@@ -3,7 +3,8 @@ $actual_link =  "http://" . $_SERVER['SERVER_NAME'];
 //echo $actual_link;
 
 $file_name = 'contacts.csv';
-$file_url = 'http://" . $_SERVER["SERVER_NAME"]' . $file_name;
+$file_url =  $actual_link . $file_name;
+
 header('Content-type: text/csv');
 //header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"".$file_name."\""); 
