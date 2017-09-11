d08c193ef7ca59c94deb7f5991b1503c3949daef
baseline <websolution806@gmail.com>
2017-09-12 02:38:44 +0530

    Update download.php

--- download.php
+++ download.php
@@ -3,7 +3,7 @@ $actual_link =  "http://" . $_SERVER['SERVER_NAME'];
 //echo $actual_link;
 
 $file_name = 'contacts.csv';
-$file_url = 'http://dev.seventhfoundation.com/instagram/' . $file_name;
+$file_url = "'.$actual_link.'/download.php" . $file_name;
 header('Content-type: text/csv');
 //header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"".$file_name."\""); 
