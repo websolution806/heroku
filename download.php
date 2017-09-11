6559e6aad0f2594e57d55b0d12ae721003af6817
baseline <websolution806@gmail.com>
2017-09-12 00:09:10 +0530

    Add files via upload

---
+++ download.php
@@ -0,0 +1,12 @@
+<?php
+$actual_link =  "http://" . $_SERVER['SERVER_NAME'];
+//echo $actual_link;
+
+$file_name = 'contacts.csv';
+$file_url = 'http://dev.seventhfoundation.com/instagram/' . $file_name;
+header('Content-type: text/csv');
+//header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"".$file_name."\""); 
+readfile($file_url);
+exit;
+?>
